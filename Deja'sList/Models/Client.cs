ee29b5855e4eeea73e32dca46f49d9ae386ea1ff
Charles Erickson <cse621@gmail.com>
2019-05-13 09:19:24 -0500

    added client model

---
+++ Deja'sList/Models/Client.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace Deja_sList.Models
+{
+    public class Client
+    {
+        [Key]
+        public int ClientId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set;}
+        public string Zipcode { get; set; }
+        public string Email { get; set; }
+
+        [ForeignKey("Jobs")]
+        public string JobId { get; set; }
+        public Jobs Jobs { get; set; }
+    
+    }
+}
